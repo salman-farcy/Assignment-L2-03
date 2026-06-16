5941f3aa2d031ddaa9997c439f63f2e810f4a8a3
slaman-farcy <salmanfarcy253@gmail.com>
2026-06-16 15:14:45 +0600

    insert users data

--- QUERY.sql
+++ QUERY.sql
@@ -46,3 +46,12 @@ CREATE TABLE bookings(
     FOREIGN KEY (match_id)
     REFERENCES matches(match_id)
 );
+
+
+-- STEP 4: insert sample data in users
+INSERT INTO users (user_id, full_name, email, role, phone_number)
+VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'), 
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'), 
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'), 
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
