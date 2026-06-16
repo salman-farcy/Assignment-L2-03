c5082e38b42aedf1717f4f083c2551bf603bc803
slaman-farcy <salmanfarcy253@gmail.com>
2026-06-16 14:48:58 +0600

    user table create

--- QUERY.sql
+++ QUERY.sql
@@ -7,3 +7,13 @@ CREATE DATABASE Assignment03;
 DROP TABLE IF EXISTS bookings; 
 DROP TABLE IF EXISTS matches; 
 DROP TABLE IF EXISTS users;
+
+
+-- Step 01: create Tables (schema)
+CREATE TABLE users ( 
+     user_id INT PRIMARY KEY, 
+     full_name VARCHAR(100) NOT NULL, 
+     email VARCHAR(100) UNIQUE NOT NULL, 
+     role VARCHAR(50) NOT NULL, 
+     phone_number VARCHAR(20) 
+);
