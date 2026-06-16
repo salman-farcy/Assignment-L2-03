1f0d78291895b6cac57476c8d925ce8f2f7ebf6b
slaman-farcy <salmanfarcy253@gmail.com>
2026-06-16 16:21:34 +0600

    query 4 created

--- QUERY.sql
+++ QUERY.sql
@@ -114,3 +114,12 @@ SELECT
   COALESCE(payment_status, 'Action Required') AS systematic_status
 FROM bookings
 WHERE payment_status IS NULL;
+
+
+
+-- STEP 10 : Query 4, SQL Queries
+SELECT 
+  b.booking_id, u.full_name, m.fixture, b.total_cost
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN matches m ON b.match_id = m.match_id;
