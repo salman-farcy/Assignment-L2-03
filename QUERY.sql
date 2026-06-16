443d80e6afc2240f9ef969f6ecdcf27fb7fea79e
slaman-farcy <salmanfarcy253@gmail.com>
2026-06-16 14:55:30 +0600

    matches table created

--- QUERY.sql
+++ QUERY.sql
@@ -17,3 +17,13 @@ CREATE TABLE users (
      role VARCHAR(50) NOT NULL, 
      phone_number VARCHAR(20) 
 );
+
+
+-- Step 02: create Tables (schema)
+CREATE TABLE matches ( 
+  match_id INT PRIMARY KEY, 
+  fixture VARCHAR(150) NOT NULL, 
+  tournament_category VARCHAR(100) NOT NULL, 
+  base_ticket_price NUMERIC(10,2) NOT NULL, 
+  match_status VARCHAR(50) NOT NULL 
+);
