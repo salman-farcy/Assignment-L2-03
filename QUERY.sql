caf65866eabd5717bf8fdd561e31292cdc886188
slaman-farcy <salmanfarcy253@gmail.com>
2026-06-16 15:29:58 +0600

    insert matches data

--- QUERY.sql
+++ QUERY.sql
@@ -55,3 +55,14 @@ VALUES
 (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'), 
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'), 
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
+
+
+-- STEP 5: insert sample data in matches 
+INSERT INTO matches (match_id, fixture, tournament_category, base_ticket_price, match_status)
+VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150, 'Available'), 
+(102, 'Man City vs Liverpool', 'Premier League', 120, 'Selling Fast'), 
+(103, 'Bayern Munich vs PSG', 'Champions League', 130, 'Available'), 
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90, 'Sold Out'), 
+(105, 'Juventus vs Roma', 'Serie A', 80, 'Available');
