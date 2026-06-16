d2c839f9989293d43ce8c134dc3b9d7d3c839288
slaman-farcy <salmanfarcy253@gmail.com>
2026-06-16 16:17:57 +0600

    query 3 created

--- QUERY.sql
+++ QUERY.sql
@@ -102,3 +102,15 @@ SELECT
 FROM users
 WHERE full_name ILIKE 'Tanvir%'
   OR full_name ILIKE '%Haque%';
+
+
+
+
+-- STEP 9 : Query 3, SQL Queries
+SELECT 
+  booking_id,
+  user_id,
+  match_id,
+  COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM bookings
+WHERE payment_status IS NULL;
