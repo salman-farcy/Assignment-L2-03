b32d4536bfa3e0d0f68a054b40594c238770762a
slaman-farcy <salmanfarcy253@gmail.com>
2026-06-16 14:35:35 +0600

    optional safety step

--- QUERY.sql
+++ QUERY.sql
@@ -1,2 +1,9 @@
 -- Create Database Assignment03
-create database Assignment03;
+CREATE DATABASE Assignment03;
+
+
+
+-- Step 00: Drop Tables (optional safety step)
+DROP TABLE IF EXISTS bookings; 
+DROP TABLE IF EXISTS matches; 
+DROP TABLE IF EXISTS users;
